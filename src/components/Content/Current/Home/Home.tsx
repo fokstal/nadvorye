4849df744817655e60033fb781772e78039cdbbc
Fokstal <klabu2016@mail.ru>
2024-12-28 21:26:09 +0300

    style: change background (Sat 28.12.24 09:26 PM)

--- src/components/Content/Current/Home/Home.tsx
+++ src/components/Content/Current/Home/Home.tsx
@@ -3,7 +3,7 @@ import { convertFullDateFrom_ISO8601 } from "../../../../helpers/convertFrom_ISO
 import WeatherModel from "../../../../models/WeatherModel";
 import Language from "../../../../const/Language";
 import BurgerMenu from "../../../BurgerMenu/BurgerMenu";
-import background from "../../../../assets/images/louise_lake_mountains.jpg";
+import background from "../../../../assets/images/road_asphalt_rain.jpg";
 import getDominantColorInHex from "../../../../helpers/getDominantColor";
 import "./Home.scss";
 import Header from "../../../Header/Header";
