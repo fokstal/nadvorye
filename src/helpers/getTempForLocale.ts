771cc5af2cbeed87cff6631015e0fdfdb8bbed1c
Fokstal <klabu2016@mail.ru>
2024-12-29 12:37:14 +0300

    --amend

--- src/helpers/getTempForLocale.ts
+++ src/helpers/getTempForLocale.ts
@@ -1,9 +1,9 @@
 import Language from "../const/Language";
 import celsiusToFahrenheit from "./celsiusToFahrenheit";
 
-const getTempForLocale = (temp_c: number, lang = Language.RU) => {
-    if ((lang = Language.EN)) return celsiusToFahrenheit(temp_c);
+const getTempForLocale = (temp_c: number, lang = Language.RU): string => {
+    if ((lang = Language.EN)) return `${celsiusToFahrenheit(temp_c)}&deg;F`;
 
-    return temp_c;
+    return `${temp_c}&deg;C`;
 };
 export default getTempForLocale;
