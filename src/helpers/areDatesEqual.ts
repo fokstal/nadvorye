dab0008c4160b906f27f4260e3bad808f46f47cd
Fokstal <klabu2016@mail.ru>
2025-01-07 17:51:04 +0300

    feat: add method for equal dates (Tue 07.01.25 05:51 PM)

---
+++ src/helpers/areDatesEqual.ts
@@ -0,0 +1,9 @@
+const areDatesEqual = (date1: Date, date2: Date) => {
+    return (
+        date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear()
+    );
+};
+
+export default areDatesEqual;
