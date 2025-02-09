96f1a56707f378caacc6aa0bf182a9adf4fee3a9
Fokstal <klabu2016@mail.ru>
2025-02-09 19:45:05 +0300

    feat: create context for 'staticData' in app (Sun 09.02.25 07:45 PM)

--- src/components/app/AppContext.ts
+++ src/components/app/AppContext.ts
@@ -1,17 +1,20 @@
 import { createContext, useContext } from "react";
 import Language from "@const/Language";
 import Theme from "@const/Theme";
+import WeatherDataJSONModel from "@models/WeatherDataJSONModel";
 
 interface IAppContext {
     city: string;
     language: Language;
     theme: Theme;
     dominantColor: string;
+    staticData: WeatherDataJSONModel;
     allowApi: boolean;
     changeCity: (newCity: string) => void;
     changeLanguage: (newLanguage: Language) => void;
     changeTheme: (newTheme: Theme) => void;
     changeDominantColor: (newDominantColor: string) => void;
+    changeStaticData: (newStaticData: WeatherDataJSONModel) => void;
     toggleAllowApi: () => void;
 }
 
--- src/components/app/AppProvider.tsx
+++ src/components/app/AppProvider.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode, useReducer } from "react";
+import data_Clear from "@assets/data/_Clear";
 import Language from "@const/Language";
 import Theme from "@const/Theme";
+import WeatherDataJSONModel from "@models/WeatherDataJSONModel";
 import { AppContext } from "./AppContext";
 
 interface IAppProvider {
@@ -12,6 +14,7 @@ enum AppReducerType {
     CHANGE_LANGUAGE = "CHANGE_LANGUAGE",
     CHANGE_THEME = "CHANGE_THEME",
     CHANGE_DOMINANT_COLOR = "CHANGE_DOMINANT_COLOR",
+    CHANGE_STATIC_DATA = "CHANGE_STATIC_DATA",
     TOGGLE_ALLOW_API = "TOGGLE_ALLOW_API",
 }
 
@@ -20,6 +23,7 @@ const initialState = {
     language: Language.RU,
     theme: Theme.WHITE,
     dominantColor: "transparent",
+    staticData: data_Clear,
     allowApi: false,
 };
 
@@ -29,6 +33,7 @@ interface AppAction {
     language?: Language;
     theme?: Theme;
     dominantColor?: string;
+    staticData?: WeatherDataJSONModel;
 }
 
 const reducer = (state: typeof initialState, action: AppAction) => {
@@ -53,6 +58,11 @@ const reducer = (state: typeof initialState, action: AppAction) => {
                 ...state,
                 dominantColor: action.dominantColor || initialState.dominantColor,
             };
+        case AppReducerType.CHANGE_STATIC_DATA:
+            return {
+                ...state,
+                staticData: action.staticData || initialState.staticData,
+            };
         case AppReducerType.TOGGLE_ALLOW_API:
             return {
                 ...state,
@@ -76,6 +86,9 @@ const AppProvider: FC<IAppProvider> = ({ children }) => {
     const changeDominantColor = (newDominantColor: string): void =>
         dispatch({ type: AppReducerType.CHANGE_DOMINANT_COLOR, dominantColor: newDominantColor });
 
+    const changeStaticData = (newStaticData: WeatherDataJSONModel): void =>
+        dispatch({ type: AppReducerType.CHANGE_STATIC_DATA, staticData: newStaticData });
+
     const toggleAllowApi = (): void => dispatch({ type: AppReducerType.TOGGLE_ALLOW_API });
 
     return (
@@ -86,6 +99,7 @@ const AppProvider: FC<IAppProvider> = ({ children }) => {
                 changeLanguage,
                 changeTheme,
                 changeDominantColor,
+                changeStaticData,
                 toggleAllowApi,
             }}
         >
