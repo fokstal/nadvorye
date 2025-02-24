abcad287e67d0e06e29051be7f7d5ccdb15b36d2
Fokstal <klabu2016@mail.ru>
2025-02-24 06:24:12 +0300

    fix: attr naming by React queries (Mon 24.02.25 06:24 AM)

--- src/components/svg/dataSVG.tsx
+++ src/components/svg/dataSVG.tsx
@@ -6,7 +6,7 @@ const DataSVG = forwardRef<SVGSVGElement, IComponentSVG>(
         return (
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" ref={ref} {...props}>
                 <mask id="ipSData0">
-                    <g fill={fill} stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+                    <g fill={fill} stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4">
                         <path d="M44 11v27c0 3.314-8.954 6-20 6S4 41.314 4 38V11" />
                         <path d="M44 29c0 3.314-8.954 6-20 6S4 32.314 4 29m40-9c0 3.314-8.954 6-20 6S4 23.314 4 20" />
                         <ellipse cx="24" cy="10" fill="#fff" rx="20" ry="6" />
